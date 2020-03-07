423b32912caaa6bd3d94a30337707573b562b45a
浙安 <2363918549@qq.com>
2020-03-07 20:16:57 +0800

    updated with victory graphics (fireworks)

--- src/game.java
+++ src/game.java
@@ -1,32 +1,38 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.applet.Applet;
 import java.applet.AudioClip;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.util.EventListener;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Random;
 import java.util.concurrent.Semaphore;
 import java.util.regex.*;
 
 public class game extends JFrame implements MouseListener, MouseMotionListener,ActionListener {
     public static final int LINES = 15;
-    public static final int SIZEX = 700;
-    public static final int SIZEY = 700;
+    public static final int SIZEX = 600;
+    public static final int SIZEY = 600;
     public static final int SPACE = 50;
     public static final int PIECERADIUS = 30;
 
-    public static boolean blackTurn = true;
+    public static boolean blackTurn;
     public static int gameState = 0;
 
     public static LinkedList<pieces> gamePieces;
     public static specialPiece mousePiece;
 
+    public static Random randomDefiniteSeed = new Random();
+
+    public static Semaphore s = new Semaphore(0);
+
     public static void main(String[] args) throws InterruptedException{
         new game();
     }
-
-    public static Semaphore s = new Semaphore(0);
 
     public game() throws InterruptedException{
         this.setLayout(null);
@@ -50,55 +56,75 @@ public class game extends JFrame implements MouseListener, MouseMotionListener,A
 
         s.acquire();
 
-        Button restartButton = new Button("Restart");
+        JButton restartButton = new JButton("Restart");
         int buttonHeight = 30;
         int buttonWidth = 50;
         restartButton.setLocation(SIZEX - buttonWidth - 10
                 ,SIZEY - SPACE*6/8 - buttonHeight);
         restartButton.setSize(buttonWidth,buttonHeight);
         restartButton.addActionListener(this);
-        this.getContentPane().add(restartButton);
+        restartButton.setEnabled(true);
+        this.add(restartButton);
 
-        Button undoButton = new Button("Undo");
+        JButton undoButton = new JButton("Undo");
         undoButton.setLocation( buttonWidth + 10
                 ,SIZEY - SPACE*6/8 - buttonHeight);
         undoButton.setSize(buttonWidth,buttonHeight);
         undoButton.addActionListener(this);
-        this.getContentPane().add(undoButton);
+        undoButton.setEnabled(true);
+        this.add(undoButton);
+        //Timer t = new Timer(1000,this);
 
         //Game loop
         while(true){
             switch(gameState) {
                 case (0)://reset, initialize
+                    //t.stop();
+
+                    //Set up playing game state 1 settings
                     blackTurn = true;
                     gamePieces = new LinkedList<>();
                     mousePiece = specialPiece.getInstance();
-                    //gamePieces.add(new pieces("black",1,2));
+                        //gamePieces.add(new pieces("black",1,2));
+
+                    //Victory state game 2 settings
                     setUpVictoryCheck();
+                    victoryBoardImage = null;
+                    fireworkTimer = 0;
+                    fireworkList = new LinkedList<>();
+                    //Redraw the board
                     repaint();
                     s.acquire();
+
+                    //Transition to gameState 2 immediately
                     gameState = 1;
+                    this.setTitle(String.valueOf(gameState));
                     break;
                 case (1)://playings
                     s.acquire();
                     if(!gamePieces.isEmpty() && checkVictory()){
                         gameState = 2;
                     }
+                    this.setTitle(String.valueOf(gameState));
                     break;
                 case (2)://Victory state
                     this.setTitle(gameState
                             + ": " + (!blackTurn? "black":"white") + " victory!");
-                    Thread.sleep(1000);
+                    repaint();
+                    Thread.sleep(100);
+                    //t.start();
                     break;
                 default:
                     break;
             }
-            this.setTitle(String.valueOf(gameState));
+
         }
     }
 
    private final static Pattern blackVictory = Pattern.compile(".*bbbbb.*");
    private final static Pattern whiteVictory = Pattern.compile(".*wwwww.*");
+
+   private static Image victoryBoardImage;
 
    private static char[][] SituationHorz;
    private static char[][] SituationVert;
@@ -205,8 +231,12 @@ public class game extends JFrame implements MouseListener, MouseMotionListener,A
 
     //public static Graphics myGraphics;
 
-    public void paintBoard(){
-       Graphics g = this.getGraphics();
+    public void paintBoard() {
+        Graphics g = this.getGraphics();
+        paintBoard(g);
+    }
+
+    public void paintBoard(Graphics g){
         g.setColor(new Color(255, 226, 76));//board color
         g.fillRect(0, 0, SIZEX, SIZEY);//Game board
         g.setColor(Color.black);//Line color
@@ -219,6 +249,8 @@ public class game extends JFrame implements MouseListener, MouseMotionListener,A
                     SPACE + i * (SIZEY - SPACE * 2) / LINES, SIZEY - SPACE);
         }
     }
+
+
     //Initial paint of the board layout
     public void paint(Graphics g){
         if(gameState == 0) {
@@ -232,11 +264,35 @@ public class game extends JFrame implements MouseListener, MouseMotionListener,A
                 p.draw(g);
             }
         }
-
         else{//victory state
-            g.drawString(blackTurn?"black":"white" + " victory!",SIZEX/2,SIZEY/2);
+            if(victoryBoardImage == null){
+                victoryBoardImage = this.createImage(game.SIZEX,game.SIZEY);
+                Graphics victoryG = victoryBoardImage.getGraphics();
+                paintBoard(victoryG);
+                for(pieces p:gamePieces){
+                    p.draw(victoryG);
+                }
+            }
+            g.drawImage(victoryBoardImage,0,0,game.SIZEX,game.SIZEY,null);
+            victoryFireworks(g);
         }
+    }
 
+    //Fireworks to generate
+    private LinkedList<fireworkHead> fireworkList;
+
+    private int fireworkTimer;
+    public void victoryFireworks(Graphics g){
+        if(fireworkTimer % 30 == 0){ //Every 3 seconds
+            fireworkList.addLast(new fireworkHead());
+        }
+        if(fireworkTimer % 500 == 499){
+            fireworkList.removeFirst();
+        }
+        fireworkTimer++;
+        for(fireworkHead f: fireworkList){
+            f.draw(g);
+        }
     }
 
     public void updateBoard(Graphics g,int X, int Y) {
@@ -337,6 +393,14 @@ public class game extends JFrame implements MouseListener, MouseMotionListener,A
 
     public void actionPerformed(ActionEvent e){
         switch(e.getActionCommand()){
+            /*
+            case "t":
+                if(gameState == 2){
+                    System.out.println("timer ticks");
+                    repaint();
+                }
+                break
+             */
             case "Restart":
                 gameState = 0;
                 repaint();
@@ -386,7 +450,6 @@ public class game extends JFrame implements MouseListener, MouseMotionListener,A
      */
     public void mouseDragged(MouseEvent e){}
 }
-
 
 abstract class genericPiece{
     int x;
@@ -468,4 +531,95 @@ class specialPiece extends genericPiece{
 
          */
     }
+}
+
+class fireworkParticle implements Cloneable{
+    //private static fireworkHead singletonHead = new fireworkHead();
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+
+    protected int RADIUS = 10;
+
+    protected int[] RGB;
+
+    public fireworkParticle(int x,int y, int dx, int dy){
+        this(x,y);
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+
+    public fireworkParticle(int x,int y){
+        RGB = new int[3];
+        this.y = y;
+        this.x = x;
+        //Set a random color
+        Random r = game.randomDefiniteSeed;
+        RGB[0] = Math.abs(r.nextInt() % 256);
+        RGB[1] = Math.abs(r.nextInt() % 256);
+        RGB[2] = Math.abs(r.nextInt() % 256);
+    }
+
+    public void draw(Graphics g){
+        g.setColor(new Color(RGB[0],RGB[1],RGB[2]));
+        g.fillOval(x-RADIUS/2,y-RADIUS/2,RADIUS,RADIUS);
+        y += dy;//Speed
+        x += dx;
+    }
+
+    @Override
+    public Object clone(){
+        fireworkParticle p = null;
+        try{
+            p = (fireworkParticle) super.clone();
+        }
+        catch(CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
+
+class fireworkHead extends fireworkParticle{
+
+    private HashSet<fireworkParticle> fireworkParticleSet;
+    public static final int numberOfParticles = 17;
+    private static final Double particleVelocity = 10.0;
+    private static final int headVelocity = 20;
+
+    public fireworkHead(){
+        this(Math.abs(game.randomDefiniteSeed.nextInt() % game.SIZEX),game.SIZEY);
+    }
+
+    public fireworkHead(int x, int y){
+        super(x,y,0,-headVelocity);
+        fireworkParticle fireworkParticleDefinite = new fireworkParticle(x,game.SIZEY/2);
+        fireworkParticleSet = new HashSet<>();
+        for(int i = 0; i < numberOfParticles; i++){
+            fireworkParticle p = (fireworkParticle) fireworkParticleDefinite.clone();
+            //new fireworkParticle(x,game.SIZEY/2);//
+            Double angle = (i*6.28)*(1.0/numberOfParticles);
+            p.dx = (int) Math.ceil(particleVelocity*Math.cos(angle));
+            p.dy = (int) Math.ceil(particleVelocity*Math.sin(angle));
+
+            fireworkParticleSet.add(p);
+        }
+        //Set the size of the head
+        RADIUS = 30;
+    }
+
+    public void draw(Graphics g){
+        if(y < game.SIZEY/2){//firework has been set-off long enough to now explode
+            for(fireworkParticle p:fireworkParticleSet ){
+                p.draw(g);
+                p.dy += 1; //Add in gravity's effect
+            }
+        }
+        else{
+            super.draw(g);
+        }
+    }
+
 }
